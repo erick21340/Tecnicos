92b4f818ae00c862bc79b4aa1f903986bc483d14
erick21340 <Torot0086@gmail.com>
2025-01-12 18:01:54 -0900

    creando el DAO

--- app/src/main/java/com/example/registrotecnicos/MainActivity.kt
+++ app/src/main/java/com/example/registrotecnicos/MainActivity.kt
@@ -11,9 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Upsert
 import com.example.registrotecnicos.ui.theme.RegistroTecnicosTheme
+import kotlinx.coroutines.flow.Flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +29,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Entity(tableName = " Registro Tecnicos")
+@Entity(tableName = " Tecnicos")
 data class TecnicosEntity(
     @PrimaryKey
     val TecnicoId: Int? = null,
     val tecnicos: String = "",
     val sueldo: Double = 0.0
 )
+@Dao
+interface TecnicoDao {
+
+
+    @Insert
+    suspend fun save(tecnico: TecnicosEntity)
+
+
+    @Query("SELECT * FROM ` tecnicos` WHERE TecnicoId = :id")
+    suspend fun find(id: Int): TecnicosEntity?
+
+
+    @Delete
+    suspend fun delete(tecnico: TecnicosEntity)
+
+
+    @Query("SELECT * FROM ` tecnicos`")
+    fun getAll(): Flow<List<TecnicosEntity>>
+}
